fbcbef309305c627325a489d53476ff376f780a8
jesster2k10 <jesster2k10@users.noreply.github.com>
2019-08-16 10:48:18 +0100

    Rails 5 support

--- config/initializers/rails/active_record/connection_adapters/quoting.rb
+++ config/initializers/rails/active_record/connection_adapters/quoting.rb
@@ -3,16 +3,18 @@
 # crash.
 # Override AR::ConnectionAdapters::Quoting to forward a SimpleDelegator's object to be quoted.
 
+module QuoteWithSimpleDelegator
+  def quote(value, column = nil)
+    return value.quoted_id if value.respond_to?(:quoted_id)
+    value = value.__getobj__ if value.is_a?(SimpleDelegator)
+    super
+  end
+end
+
 module ActiveRecord
   module ConnectionAdapters
     module Quoting
-      def quote_with_simple_delegator(value, column = nil)
-        return value.quoted_id if value.respond_to?(:quoted_id)
-        value = value.__getobj__ if value.is_a?(SimpleDelegator)
-        quote_without_simple_delegator(value, column)
-      end
-
-      prepend :quote, :simple_delegator
+      prepend QuoteWithSimpleDelegator
     end
   end
 end
